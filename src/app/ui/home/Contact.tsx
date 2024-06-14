f530590ed9192a7d666f859dfdd0cf2990bdcb69
Asmita <Asmitadhole@vionsys.com>
2024-06-14 16:19:59 +0530

    Working on Service & testimonial section of home page

--- src/app/ui/home/Contact.tsx
+++ src/app/ui/home/Contact.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 function Contact() {
   return (
@@ -9,11 +11,13 @@ function Contact() {
             Interested in collaborating with us?
           </h2>
           <h6 className="text-SubHeading font-SubHeading">Enhance your temple project with our expertise for sustainable and sacred growth.</h6>
-          <p className="text-Paragraph font-Paragraph py-3">
+          <p className="text-Paragraph font-Paragraph py-3 mb-6">
             Partner with us to create a temple that blends architectural innovation with cultural
             heritage, integrating eco-friendly practices and inspiring spiritual devotion.
           </p>
-          <button type="submit" className="px-4 py-1 bg-yellow hover:bg-[#deb01d] text-black rounded-md text-buttonText font-buttonText">Let&apos;s Get Started</button>
+          <Link href="/contact">
+            <Button type="submit" variant="outline" className="px-4 py-1 bg-yellow hover:bg-[#deb01d] text-black rounded-md text-buttonText font-buttonText">Let&apos;s Get Started</Button>
+          </Link>
         </div>
       </div>
     </div>
