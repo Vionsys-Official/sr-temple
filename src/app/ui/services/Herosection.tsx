c1c0be0516b8ef442a147e8b4c378131e27e6710
RamanPatil68 <Ramanpatil@vionsys.com>
2024-06-17 10:17:22 +0530

    Herosection Updated

--- src/app/ui/services/Herosection.tsx
+++ src/app/ui/services/Herosection.tsx
@@ -3,36 +3,33 @@ import React from 'react';
 
 function Herosection() {
   return (
-
-    <section className="relative ">
-      <div className="relative h-[60vh] md:h-[90vh] overflow-x-hidden">
+    <section className="relative">
+      <div className="relative h-[60vh] md:h-[90vh] overflow-x-hidden bg-slate-950">
         <Image
-          src="/assets/services/hero.jpg"
-          className="absolute inset-0 object-cover w-full h-full"
+          src="/assets/services/hero1.jpg"
+          className="absolute inset-0 object-cover w-full h-full opacity-60"
           alt="services"
-          height={1000}
           width={1000}
+          height={1000}
+          objectFit="cover"
         />
-        <div className="relative bg-gray bg-opacity-15 h-full flex justify-end">
-          <div className="flex items-center justify-center w-1/2">
-            <div className="flex flex-col px-4">
-              <div className="w-full p-4 text-center rounded-xl bg-white shadow-inner shadow-blue-600">
-                <h2 className=" mb-4 font-sans font-bold text-black text-4xl">
-                  Our Services
-                </h2>
-                <p className=" text-gray-400 text-lg">
-                  We offer a comprehensive range of services to ensure your
-                  temple construction project is a success from start to finish.
-                  Our expert team is dedicated to providing high-quality, reliable,
-                  and cost-effective solutions tailored to your needs.
-                </p>
-              </div>
+        <div className="absolute inset-0 flex justify-center items-center">
+          <div className="flex flex-col px-4 text-center">
+            <div className="max-w-5xl p-6 rounded-lg bg-black bg-opacity-70 shadow-lg">
+              <h2 className="mb-4 font-extrabold text-amber-500 text-3xl md:text-5xl">
+                Our Services
+              </h2>
+              <p className="text-white text-xl md:text-2xl leading-relaxed">
+                We offer a comprehensive range of services to ensure your temple
+                construction project is a success from start to finish. Our expert
+                team is dedicated to providing high-quality, reliable, and cost-effective
+                solutions tailored to your needs.
+              </p>
             </div>
           </div>
         </div>
       </div>
     </section>
-
   );
 }
 
