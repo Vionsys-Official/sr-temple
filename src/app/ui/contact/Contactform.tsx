963e7dc1c74dd8070e0cbe24ba7fc6a6e2105491
ranjitgund <ranjitgund@vionsys.com>
2024-06-17 15:45:15 +0530

    Schema updated and framer motion added

--- src/app/ui/contact/Contactform.tsx
+++ src/app/ui/contact/Contactform.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable react/require-default-props */
 /* eslint-disable @typescript-eslint/no-use-before-define */
 
@@ -6,16 +7,33 @@
 import React, { useState } from 'react';
 import { cn } from '@/utils/cn';
 import { z } from 'zod';
+import { motion } from 'framer-motion';
+import fadein from '../../../utils/variants';
 import Input from '../../../components/ui/input';
 import Label from '../../../components/ui/label';
 import ContactSchema from '../../../schema/ContactSchema';
+
+// const countryCodes = [
+//   { index: '1', code: '+91', name: 'India' },
+//   { index: '2', code: '+1', name: 'United States' },
+//   { index: '3', code: '+44', name: 'United Kingdom' },
+//   { index: '4', code: '+61', name: 'Australia ' },
+//   { index: '5', code: '+33', name: 'France ' },
+//   { index: '6', code: '+995', name: 'Georgia' },
+//   { index: '7', code: '+49', name: 'Germany' },
+//   { index: '8', code: '+62', name: 'Indonesia ' },
+//   { index: '9', code: '+39', name: 'Italy' },
+//   { index: '10', code: '+81', name: 'Japan' },
+
+//   // Add more country codes as needed
+// ];
 
 export default function SignupFormDemo() {
   const [formData, setFormData] = useState({
     firstname: '',
     lastname: '',
     email: '',
-    countryCode: '',
+    // countryCode: '+1',
     mobile: '',
     subject: '',
     message: '',
@@ -24,20 +42,28 @@ export default function SignupFormDemo() {
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>,
   ) => {
     const { id, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [id]: value }));
   };
 
+  const handleMobileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    // Allow only digits and restrict to 10 digits
+    if (/^\d*$/.test(value) && value.length <= 10) {
+      setFormData((prevData) => ({ ...prevData, mobile: value }));
+    }
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const completePhoneNumber = `${formData.countryCode}${formData.mobile}`;
+    const completePhoneNumber = `${formData.mobile}`;
     const dataToValidate = { ...formData, mobile: completePhoneNumber };
 
     try {
       ContactSchema.parse(dataToValidate);
-      // If validation is successful
+      // Output on console
       console.log('Validation successful', dataToValidate);
     } catch (err) {
       if (err instanceof z.ZodError) {
@@ -52,10 +78,18 @@ export default function SignupFormDemo() {
   };
 
   return (
+    // main section
     <section className="h-full w-full xs:py-5">
-      <h1 className="flex justify-center text-MainHeading font-MainHeading py-2 ">
+      <motion.h1
+        variants={fadein('down', 0.5)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.5 }}
+        className="flex justify-center text-MainHeading font-MainHeading py-2 "
+      >
         Connect Here
-      </h1>
+      </motion.h1>
+      {/* Form Heading */}
       <div className="md:max-w-[50vw] w-full px-5 mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white ">
         <h2 className="font-CardHeading text-CardHeading text-center">
           Let Us Know
@@ -63,27 +97,25 @@ export default function SignupFormDemo() {
         <p className="text-Paragraph font-Paragraph text-center">
           We truly appreciate your interest and look forward to assisting you
         </p>
-        {/* /Form */}
+        {/* Contact Form */}
         <form className="py-5" onSubmit={handleSubmit}>
           <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            {/* Firstname */}
             <LabelInputContainer>
               <Label htmlFor="firstname">First name</Label>
               <Input
                 id="firstname"
-                placeholder="Tyler"
+                placeholder="karan"
                 type="text"
                 value={formData.firstname}
                 onChange={handleInputChange}
               />
               {errors.firstname && <ErrorText>{errors.firstname}</ErrorText>}
             </LabelInputContainer>
-            {/* Lastname */}
             <LabelInputContainer>
               <Label htmlFor="lastname">Last name</Label>
               <Input
                 id="lastname"
-                placeholder="Durden"
+                placeholder="patil"
                 type="text"
                 value={formData.lastname}
                 onChange={handleInputChange}
@@ -91,7 +123,6 @@ export default function SignupFormDemo() {
               {errors.lastname && <ErrorText>{errors.lastname}</ErrorText>}
             </LabelInputContainer>
           </div>
-          {/* Email */}
           <LabelInputContainer className="mb-4 mt-2">
             <Label htmlFor="email">Email Address</Label>
             <Input
@@ -103,22 +134,39 @@ export default function SignupFormDemo() {
             />
             {errors.email && <ErrorText>{errors.email}</ErrorText>}
           </LabelInputContainer>
-          {/* Mobile number */}
           <LabelInputContainer className="mb-4 mt-2">
             <Label htmlFor="mobile">Mobile Number</Label>
             <div className="">
+              {/* <select
+                id="countryCode"
+                value={formData.countryCode}
+                onChange={handleInputChange}
+                className="border rounded-md"
+              >
+                <option value=""> Select country</option>
+                {countryCodes.map((country) => (
+                  <option key={country.index} value={country.code}>
+                    {country.name}
+
+                    {country.code}
+
+                  </option>
+                ))}
+              </select> */}
               <Input
                 id="mobile"
-                placeholder="234 567 8901"
+                placeholder="Mobile number"
                 type="tel"
                 className="w-full"
                 value={formData.mobile}
-                onChange={handleInputChange}
+                inputMode="numeric"
+                pattern="\d*"
+                maxLength={10}
+                onChange={handleMobileInput}
               />
             </div>
             {errors.mobile && <ErrorText>{errors.mobile}</ErrorText>}
           </LabelInputContainer>
-          {/* Subject */}
           <LabelInputContainer className="mb-8">
             <Label htmlFor="subject">Subject</Label>
             <Input
@@ -130,13 +178,13 @@ export default function SignupFormDemo() {
             />
             {errors.subject && <ErrorText>{errors.subject}</ErrorText>}
           </LabelInputContainer>
-          {/* Message */}
           <LabelInputContainer className="mb-8">
             <Label htmlFor="message">Message</Label>
             <textarea
               className="border border-slate-400 rounded-md"
               id="message"
               name="message"
+              placeholder="  message here"
               rows={4}
               cols={50}
               value={formData.message}
--- src/app/ui/contact/Getintouch.tsx
+++ src/app/ui/contact/Getintouch.tsx
@@ -1,6 +1,9 @@
-/* eslint-disable react/self-closing-comp */
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
+import fadein from '../../../utils/variants';
 import call from '../../../../public/assets/contact/call-service.gif';
 import email from '../../../../public/assets/contact/email-file.gif';
 import location from '../../../../public/assets/contact/location-pin.gif';
@@ -8,16 +11,23 @@ import location from '../../../../public/assets/contact/location-pin.gif';
 function GetInTouch() {
   return (
     <div className="w-screen">
-      <section className=" h-[80vh] w-screen">
+      <section className=" h-[80vh] w-screen ">
         <div className="flex-col text-center  justify-center py-5">
           <div className="text-center">
-            <h1 className="text-MainHeading font-MainHeading">
+            <motion.h1
+              variants={fadein('up', 0.5)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.5 }}
+              className="text-MainHeading font-MainHeading"
+            >
               Contact Our Office
-            </h1>
+            </motion.h1>
           </div>
-          {/* first card */}
+          {/* cards */}
           <div className="flex md:flex-row flex-col justify-center gap-6 md:gap-10 py-10">
-            <div className="cursor-default overflow-hidden relative transition-all duration-500 hover:translate-y-2 md:w-[27vw] md:h-[50vh] h-[20vh] bg-neutral-50 rounded-lg shadow-xl flex items-center justify-center gap-2 p-4 before:absolute before:w-full hover:before:top-0 before:duration-500 before:-top-1 before:h-1 before:bg-amber-400">
+            {/* First card */}
+            <div className="cursor-default overflow-hidden relative transition-all duration-500 hover:translate-y-2 md:w-[27vw] md:h-[50vh] h-[20vh] bg-neutral-50 rounded-lg shadow-md shadow-slate-500 flex items-center justify-center gap-2 p-4 before:absolute before:w-full hover:before:top-0 before:duration-500 before:-top-1 before:h-1 before:bg-amber-400">
               <div className="flex flex-col justify-center items-center text-center w-full">
                 <Image src={location} alt="icon" className="w-12 h-12 mb-4" />
                 <span className="font-CardHeading text-CardHeading">
@@ -30,7 +40,7 @@ function GetInTouch() {
             </div>
 
             {/* Second Card */}
-            <div className="cursor-default overflow-hidden relative transition-all duration-500 hover:translate-y-2 md:w-[27vw] md:h-[50vh] h-[20vh] bg-neutral-50 rounded-lg shadow-xl flex items-center justify-center gap-2 p-4 before:absolute before:w-full hover:before:top-0 before:duration-500 before:-top-1 before:h-1 before:bg-amber-400">
+            <div className="cursor-default overflow-hidden relative transition-all duration-500 hover:translate-y-2 md:w-[27vw] md:h-[50vh] h-[20vh] bg-neutral-50 rounded-lg shadow-md shadow-slate-500 flex items-center justify-center gap-2 p-4 before:absolute before:w-full hover:before:top-0 before:duration-500 before:-top-1 before:h-1 before:bg-amber-400">
               <div className="flex flex-col justify-center items-center text-center w-full">
                 <Image src={email} alt="icon" className="w-12 h-12 mb-4" />
                 <span className="font-CardHeading text-CardHeading">Email</span>
@@ -41,7 +51,7 @@ function GetInTouch() {
               </div>
             </div>
             {/* third card */}
-            <div className="cursor-default overflow-hidden relative transition-all duration-500 hover:translate-y-2 md:w-[27vw] md:h-[50vh] h-[20vh] bg-neutral-50 rounded-lg shadow-xl flex items-center justify-center gap-2 p-4 before:absolute before:w-full hover:before:top-0 before:duration-500 before:-top-1 before:h-1 before:bg-amber-400">
+            <div className="cursor-default overflow-hidden relative transition-all duration-500 hover:translate-y-2 md:w-[27vw] md:h-[50vh] h-[20vh] bg-neutral-50 rounded-lg shadow-md shadow-slate-500 flex items-center justify-center gap-2 p-4 before:absolute before:w-full hover:before:top-0 before:duration-500 before:-top-1 before:h-1 before:bg-amber-400">
               <div className="flex flex-col justify-center items-center text-center w-full">
                 <Image src={call} alt="icon" className="w-12 h-12 mb-4" />
                 <span className="font-CardHeading text-CardHeading">
