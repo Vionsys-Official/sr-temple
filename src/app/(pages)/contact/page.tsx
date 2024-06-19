fb34ef5d9ed804243994ac3791c3287a9c63ef8d
ranjitgund <ranjitgund@vionsys.com>
2024-06-19 11:28:17 +0530

    Meta data added and change in address.

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -4,6 +4,9 @@ import Getintouch from '../../ui/contact/Getintouch';
 import Addressmap from '../../ui/contact/Addressmap';
 import Contactform from '../../ui/contact/Contactform';
 
+export const metadata = {
+  title: 'SRTemple Constructions - Contact Us',
+};
 const page = () => (
   <div className="overflow-x-hidden">
     <Hero />
