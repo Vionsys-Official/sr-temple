41c4563b11d4967a3ea825d9475c8472e1ccddaf
SahilBankar-18 <dev321.net@gmail.com>
2024-06-19 11:23:41 +0530

    Landing Page Folder structure created and Meta-data added in portfolio page.

---
+++ src/app/(LANDING_PAGE)/landingpage/components/CounterSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CounterSection() {
+  return (
+    <div />
+  );
+}
+
+export default CounterSection;
---
+++ src/app/(LANDING_PAGE)/landingpage/components/WhyChooseUs.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function WhyChooseUs() {
+  return (
+    <div />
+  );
+}
+
+export default WhyChooseUs;
