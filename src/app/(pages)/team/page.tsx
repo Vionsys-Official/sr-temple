0f761c3c1940b5dea5fa0cbe07215c45268ab666
Vansh-Vionsys <Technowebdev045454>
2024-06-24 11:28:04 +0530

    Meta Data Updated on Pages

--- src/app/(pages)/team/page.tsx
+++ src/app/(pages)/team/page.tsx
@@ -3,10 +3,13 @@ import Section2 from '@/app/ui/team/Section2';
 import TeamHeroSetion from '@/app/ui/team/TeamHeroSetion';
 import Section3 from '@/app/ui/team/Section3';
 import Section4 from '@/app/ui/team/Section4';
+import { Metadata } from 'next';
 
-export const metadata = {
-  title: 'SR Temple Constructions - Teams Page',
+export const metadata: Metadata = {
+  title: 'Teams',
+  description: 'Meet our dedicated team of skilled professionals who bring decades of experience and expertise to every project. Our talented architects, engineers, craftsmen, and support staff work collaboratively to ensure the highest standards of quality and precision in temple construction. Learn more about the individuals who are committed to making your vision a reality.',
 };
+
 function Page() {
   return (
     <section>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,12 @@ import Footer from './ui/footer/Footer';
 import Navbar from './ui/navbar/Navbar';
 
 export const metadata: Metadata = {
-  title: 'SRTemple Construction Pvt. Ltd.',
-  description: 'Welcome to the SRTemple Construction Pvt. Ltd.',
+  title: {
+    default: 'SR Temple Construction Pvt. Ltd.',
+    template: '%s - SR Temple Construction Pvt. Ltd.',
+  },
+  description:
+  'Welcome to SR Temple Construction! With decades of experience and a team of skilled professionals, we offer unparalleled solutions for all your temple construction needs. As a leading provider of high-quality temple construction services, we are dedicated to ensuring excellence and precision in every project we undertake. Explore our expertise and commitment to building sacred spaces that inspire and elevate.',
 };
 
 export default function RootLayout({
