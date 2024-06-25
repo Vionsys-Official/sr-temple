8ea281bd455cb0028dffbdc40321475637d7c700
Vansh-Vionsys <Technowebdev045454>
2024-06-25 18:46:44 +0530

    Meatadata Updated and Sitemap and robots and opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@ import { GeistSans } from 'geist/font/sans';
 import './globals.css';
 import React from 'react';
 
-const BaseUrl = process.env.DOMAIN;
-
+const BaseUrl = process.env.domain;
 export const metadata: Metadata = {
   metadataBase: new URL(`${BaseUrl}`),
   title: {
@@ -13,8 +12,7 @@ export const metadata: Metadata = {
   },
   description:
   'Welcome to SR Temple Construction! With decades of experience and a team of skilled professionals, we offer unparalleled solutions for all your temple construction needs. As a leading provider of high-quality temple construction services, we are dedicated to ensuring excellence and precision in every project we undertake. Explore our expertise and commitment to building sacred spaces that inspire and elevate.',
-  openGraph:
-  {
+  openGraph: {
     images: '/src/app/opengraph-image.png',
   },
   twitter: {
