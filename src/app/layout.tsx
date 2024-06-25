9e74cc442124696f766f4eb21316fd8709704484
Vansh-Vionsys <Technowebdev045454>
2024-06-25 18:07:33 +0530

    Meta data added with sitemap,robote and opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,23 @@ import './globals.css';
 import Footer from './ui/footer/Footer';
 import Navbar from './ui/navbar/Navbar';
 
+const BaseUrl = process.env.DOMAIN;
+
 export const metadata: Metadata = {
+  metadataBase: new URL(`${BaseUrl}`),
   title: {
     default: 'SR Temple Construction Pvt. Ltd.',
     template: '%s - SR Temple Construction Pvt. Ltd.',
   },
   description:
   'Welcome to SR Temple Construction! With decades of experience and a team of skilled professionals, we offer unparalleled solutions for all your temple construction needs. As a leading provider of high-quality temple construction services, we are dedicated to ensuring excellence and precision in every project we undertake. Explore our expertise and commitment to building sacred spaces that inspire and elevate.',
+  openGraph:
+  {
+    images: `${BaseUrl}`,
+  },
+  twitter: {
+    card: 'summary_large_image',
+  },
 };
 
 export default function RootLayout({
